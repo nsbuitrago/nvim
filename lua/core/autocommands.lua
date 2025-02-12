05d0a31b1bdfa61cdeddb92a6a82ab81f38783f6
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2025-02-12 10:05:18 -0600

    add: ues relative num in norm mode/abs num in ins mode

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -11,3 +11,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  desc = 'Use absolute line numbers in insert mode',
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  desc = 'Use relaitve line numbers outside insert mode',
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
